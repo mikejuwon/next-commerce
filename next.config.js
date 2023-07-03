d73c2cb6e70456e8e72ef7c2401155759bf1c050
mikejuwon-oasis <mstephen@oasismgt.net>
2023-07-03 16:46:10 +0100

    some changes

--- next.config.js
+++ next.config.js
@@ -5,27 +5,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: [`${hostname}`],
-  },
-
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' }, 
-      '/about': { page: '/about' },
-      '/contact': { page: '/contact' },
-      '/blog': { page: '/blog' },
-      '/blog/[slug]': { page: '/blog/[slug]' },
-      '/portfolio': { page: '/portfolio' },
-      '/portfolio/[slug]': { page: '/portfolio/[slug]' },
-      '/services': { page: '/services' },
-      '/services/[slug]': { page: '/services/[slug]' },
-      '/team': { page: '/team' },
-      '/team/[slug]': { page: '/team/[slug]' },
-      '/testimonials': { page: '/testimonials' },
-      '/testimonials/[slug]': { page: '/testimonials/[slug]' },
-      '/404': { page: '/404' },
-      
-    };
-  },
+  }
 };
 
 module.exports = nextConfig;
